ed4f5d67207447299b1b3532a2502d83386a7756
ivankodrnja <ikodrnja@gmail.com>
2016-01-28 21:22:14 +0100

    added UIDatePicker functionality

--- Travelbirdie/TravelbirdieSearchViewController.swift
+++ Travelbirdie/TravelbirdieSearchViewController.swift
@@ -17,13 +17,22 @@ class TravelbirdieSearchViewController: UIViewController, UITableViewDelegate, U
     @IBOutlet weak var datePickerContainerView: UIView!
     @IBOutlet weak var tableViewContainer: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    // it will be used in UIDatePicker
+    var checkInDate : NSDate = NSDate()
+    var checkOutDate : NSDate?
+    
     // variable thata will use to enable / disable search row in the column
     var searchTapped : Bool = false
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        checkOutDate = NSCalendar.currentCalendar().dateByAddingUnit(
+            .Day,
+            value: 1,
+            toDate: self.checkInDate,
+            options: [])!
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -35,54 +44,12 @@ class TravelbirdieSearchViewController: UIViewController, UITableViewDelegate, U
         self.backgroundImage.alpha = 1.0
         activityIndicator.hidesWhenStopped = true
         activityIndicator.stopAnimating()
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func searchRentals() {
-        
-        // let the user know something is going on under the hood
-        self.tableViewContainer.alpha = 0.5
-        self.backgroundImage.alpha = 0.5
-        self.activityIndicator.startAnimating()
-        
-       let controller = storyboard!.instantiateViewControllerWithIdentifier("ResultsViewController") as! ResultsViewController
-       
-        // prevent user from submiting twice
-        self.searchTapped = true
-
-        
-        
-        let locationLat = 45.3400805
-        let locationLon = 14.3096924
-        
-        let date = NSDate().timeIntervalSince1970
-        
-        print(date)
-        
-        let checkIn = date
-        let checkOut = date + 86400
-        
-        ZilyoClient.sharedInstance().getRentals(locationLat, locationLon: locationLon, checkIn: checkIn, checkOut: checkOut){(result, error) in
-            
-            
-            if error == nil {
-                //print(result)
-                dispatch_async(dispatch_get_main_queue()) {
-                    // search row is enabled again
-                    self.searchTapped = false
-                    //controller.result = result!
-                    ZilyoClient.sharedInstance().apartmentDict = result!
-                    self.navigationController!.pushViewController(controller, animated: true)
-                }
-            }
-            
-            
-            
-        }
     }
 
     
@@ -149,15 +116,23 @@ class TravelbirdieSearchViewController: UIViewController, UITableViewDelegate, U
             break
         // select check in date
         case 2:
-            //self.tableview.alpha = 0.1
-            //self.tabBarController?.tabBar.hidden = true
-            //self.datePickerContainerView.hidden = false
-            let checkInDate = self.getDate()
+            if(searchTapped){
+                return
+            } else {
+                self.getDate("checkInDate")
             
-            print (checkInDate)
+ 
+            }
+            
         // select check out date
         case 3:
-            break
+            if(searchTapped){
+                return
+            } else {
+                self.getDate("checkOutDate")
+
+            }
+            
             
         case 4:
             // if the user already tapped search rentals
@@ -176,25 +151,111 @@ class TravelbirdieSearchViewController: UIViewController, UITableViewDelegate, U
     
     // MARK: - Helper functions
     
-    func getDate(){
+    
+    func searchRentals() {
         
-        // configure done button
-        let doneButton = UIButton()
-        doneButton.setTitle("Done", forState: UIControlState.Normal)
-        dateSelectionPicker.addSubview(doneButton)
+        // let the user know something is going on under the hood
+        self.tableViewContainer.alpha = 0.5
+        self.backgroundImage.alpha = 0.5
+        self.activityIndicator.startAnimating()
         
-        //dateSelectionPicker.hidden = false
+        let controller = storyboard!.instantiateViewControllerWithIdentifier("ResultsViewController") as! ResultsViewController
+        
+        // prevent user from submiting twice
+        self.searchTapped = true
+        
+        
+        
+        let locationLat = 45.3400805
+        let locationLon = 14.3096924
+        
+        let date = NSDate().timeIntervalSince1970
+        
+        print(date)
+        
+        let checkIn = date
+        let checkOut = date + 86400
+        
+        ZilyoClient.sharedInstance().getRentals(locationLat, locationLon: locationLon, checkIn: checkIn, checkOut: checkOut){(result, error) in
+            
+            
+            if error == nil {
+                //print(result)
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    //controller.result = result!
+                    ZilyoClient.sharedInstance().apartmentDict = result!
+                    self.navigationController!.pushViewController(controller, animated: true)
+                    // search row is enabled again
+                    self.searchTapped = false
+                }
+            }
+
+            
+        }
+    }
+    
+    func getDate(type: String){
+        
+        // put the date picker outside of the view
+        var frame: CGRect = self.datePickerContainerView.frame
+        frame.origin.y = self.view.frame.size.height
+        self.datePickerContainerView.frame = frame
         
         UIView.animateWithDuration(0.4, animations: {
             
+            // manage the appearance of the date picker
             self.datePickerContainerView.hidden = false
             self.tabBarController?.tabBar.hidden = true
+            
             var frame: CGRect = self.datePickerContainerView.frame
-            frame.origin.y = self.view.frame.size.height - 600.0
+            frame.origin.y = self.view.frame.size.height - frame.size.height
             self.datePickerContainerView.frame = frame
+
             self.dateSelectionPicker.hidden = false
+            
+            
+            // date picker behavior depending on the type
+            if(type=="checkInDate"){
+                self.dateSelectionPicker.minimumDate = self.checkInDate
+                self.dateSelectionPicker.addTarget(self, action: "recognizecheckInDate:", forControlEvents: UIControlEvents.AllEvents)
+                
+            } else if (type=="checkOutDate"){
+                self.dateSelectionPicker.minimumDate = self.checkOutDate
+                self.dateSelectionPicker.addTarget(self, action: "recognizecheckOutDate:", forControlEvents: UIControlEvents.AllEvents)
+            }
+            
+
+            
+      
+            
         })
         
+    }
+    
+    @IBAction func dismissPicker(sender: AnyObject) {
+        UIView.animateWithDuration(0.4, animations: {
+           
+            self.datePickerContainerView.frame = CGRectMake(self.view.frame.minX, self.view.frame.maxY, self.view.frame.width, self.datePickerContainerView.frame.height)
+            
+            let dateFormatter = NSDateFormatter()
+            
+            dateFormatter.dateFormat = "dd/MM/yyyy"
+
+            //print (dateFormatter.stringFromDate(self.dateSelectionPicker.date))
+            print("CheckIn:\(self.checkInDate)")
+            print("CheckOut:\(self.checkOutDate!)")
+            
+        })
+        
+    }
+    
+    func recognizecheckInDate(sender: UIDatePicker) {
+        self.checkInDate = sender.date
+    }
+    
+    func recognizecheckOutDate(sender: UIDatePicker) {
+        self.checkOutDate = sender.date
     }
 
 }
